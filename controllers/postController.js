285f8db3d0dc29678ff35170f9b1b9b181ddad1c
Adit Modi <adit_modi@external.mckinsey.com>
2018-07-04 10:51:56 +0530

    ash

--- controllers/postController.js
+++ controllers/postController.js
@@ -9,8 +9,6 @@ export const postData = (req, res) => {
   let valid_age = validate(/^[0-9]+$/i, req.body.age);
   let valid_emp_id = validate(/^[A-Za-z0-9]+$/i, req.body.emp_id);
   if (valid_name && valid_age && valid_phone && valid_emp_id){
-    PostData.find({ emp_id: req.body.emp_id }, (err, values) => {
-      if(err){
         let p = new PostData();
         p.name = req.body.name;
         p.emp_id = req.body.emp_id;
@@ -22,14 +20,6 @@ export const postData = (req, res) => {
           if (err){res.send(`Error in uploading: ${err}`);}
           else {res.send('Form saved');}
         }));
-      }
-      else {
-        res.send('Employee id exists');
-      }
-    })
-  }
-  else {
-    res.send('Dissolve the errors');
   }
 }
 
@@ -49,14 +39,19 @@ export const getForm = (req, res) => {
 
 export const editForm = (req, res) => {
   PostData.findOne({emp_id: req.params.id}, (err, values) => {
-    values.name = req.body.name;
-    values.dept = req.body.dept;
-    values.age = req.body.age;
-    values.gender = req.body.gender;
-    values.phone = req.body.phone;
-    values.save((error) => {
-      if(err) res.send(error);
-      else res.send('Changes updated');
-    });
+    if(err){
+      res.send(err)
+    }
+    else{
+      values.name = req.body.name;
+      values.dept = req.body.dept;
+      values.age = req.body.age;
+      values.gender = req.body.gender;
+      values.phone = req.body.phone;
+      values.save((error) => {
+        if(error) res.send(error);
+        else res.send('Changes updated');
+      });
+    }
   })
 }
